19448bb540c3f3b0ec6ea741f53188cd03f4c7f2
Prathamesh Deshmukh <deshmukhprathamesh4141@gmail.com>
2024-10-01 02:51:20 +0530

    Resolved conflicts

--- server.js
+++ server.js
@@ -1,42 +1,61 @@
 const express = require('express');
+const cors = require('cors'); // Import the CORS package
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
 
+// Use CORS middleware to enable CORS for all routes
+app.use(cors()); 
+
 // Webhook endpoint for Instagram
 app.post('/webhook/instagram', (req, res) => {
-  console.log('Instagram Webhook:', req.body);
-  console.log("This is Instagram");
-  
-  // Send a custom response back to the client
-  res.json({
-    message: 'Instagram webhook received successfully',
-    receivedData: req.body // Echoing back the received data
-  });
+  try {
+    console.log('Instagram Webhook:', req.body);
+    console.log("This is Instagram");
+
+    // Send a custom response back to the client
+    res.status(200).json({
+      message: 'Instagram webhook received successfully',
+      receivedData: req.body // Echoing back the received data
+    });
+  } catch (error) {
+    console.error('Error handling Instagram webhook:', error);
+    res.status(500).json({ message: 'Error processing Instagram webhook' });
+  }
 });
 
 // Webhook endpoint for Facebook
 app.post('/webhook/facebook', (req, res) => {
-  console.log('Facebook Webhook:', req.body);
-  
-  // Send a custom response back to the client
-  res.json({
-    message: 'Facebook webhook received successfully',
-    receivedData: req.body // Echoing back the received data
-  });
+  try {
+    console.log('Facebook Webhook:', req.body);
+
+    // Send a custom response back to the client
+    res.status(200).json({
+      message: 'Facebook webhook received successfully',
+      receivedData: req.body // Echoing back the received data
+    });
+  } catch (error) {
+    console.error('Error handling Facebook webhook:', error);
+    res.status(500).json({ message: 'Error processing Facebook webhook' });
+  }
 });
 
 // Webhook endpoint for WhatsApp
 app.post('/webhook/whatsapp', (req, res) => {
-  console.log('WhatsApp Webhook:', req.body);
-  
-  // Send a custom response back to the client
-  res.json({
-    message: 'WhatsApp webhook received successfully',
-    receivedData: req.body // Echoing back the received data
-  });
+  try {
+    console.log('WhatsApp Webhook:', req.body);
+
+    // Send a custom response back to the client
+    res.status(200).json({
+      message: 'WhatsApp webhook received successfully',
+      receivedData: req.body // Echoing back the received data
+    });
+  } catch (error) {
+    console.error('Error handling WhatsApp webhook:', error);
+    res.status(500).json({ message: 'Error processing WhatsApp webhook' });
+  }
 });
 
 // Start the server
